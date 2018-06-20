914a2bbbbbb8c9238af682f9c7a016379bd1d29d
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-06-21 00:10:07 +0200

    Add options to save a list of words

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       // ignore: expected_token
       home: new RandomWords(),
     );
@@ -24,12 +27,49 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final _saved = new Set<WordPair>();
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+          builder: (context) {
+            final tiles = _saved.map(
+                (pair) {
+                  return new ListTile(
+                    title: new Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+            );
+            final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+            )
+            .toList();
+
+
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text('Saved Suggestions'),
+              ),
+              body: new ListView(children: divided),
+            );
+          },
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
+        backgroundColor: Colors.blueAccent,
+        title: new Text('Startup Name Generator',),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
